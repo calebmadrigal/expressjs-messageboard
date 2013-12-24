4451c971a6ff191473d6741ebd5ce23d14d88ed5
Caleb Madrigal <caleb.madrigal@gmail.com>
2013-12-24 15:25:29 -0600

    Still trying to enable CORS.

--- web.js
+++ web.js
@@ -57,7 +57,7 @@ function put_message(req, res) {
 // CORS
 //////////////////////////////////////////////////////////////////////////////////// CORS MIDDLEWARE
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
